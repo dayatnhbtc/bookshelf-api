030728bb6cbcc7ec0afb28ed93dadeb90eee43d6
dayatnhbtc <dayatnhbtc@gmail.com>
2023-12-03 20:00:36 +0700

    intial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+    addBookHandler,
+    getAllBooks,
+    getBookByIdHandler,
+    updateBookByIdHandler,
+    deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooks,
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: updateBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookByIdHandler,
+    },
+];
+
+module.exports = routes;
